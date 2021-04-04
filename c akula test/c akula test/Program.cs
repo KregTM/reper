72a4adefc27996be542d05c2803388edf9b3f904
Nikolai <kreis.roma@gmail.com>
2021-04-04 15:29:40 +0600

    1 work

---
+++ c akula test/c akula test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_akula_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String source = "4567391245";
+
+            for (int i = 0; i >= source.Length; i++)
+            {
+                Console.WriteLine(source[i]);
+            }
+
+        }   
+    }
+}
